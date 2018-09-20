1d9edfee93177adfffde043007baae759d13eaf4
Vivek5545 <vivekkumarmeena5545@gmail.com>
2018-09-20 23:05:49 +0530

    almnac

--- src/duck_hunt/Credits.java
+++ src/duck_hunt/Credits.java
@@ -19,7 +19,7 @@ public class Credits extends DialogProgram{
     public void run()
     {
         Menu menu = new Menu();
-        println("             Made By     \n\n         SAHAJ BAMBA\n   VAISHNAV AGARWAL\n           VIVEK MEENA  ");
+        println("             Made By     \n\n         SAHAJ BAMBA\n   VAISHNAV AGARWAL\n           VIVEK KUMAR MEENA  ");
         menu.run();
     }
 }
---
+++ test/test1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+
+    
+import java.awt.FlowLayout;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.JPanel;
+import javax.swing.Jpanel;  
+public class test1 {  
+    public static void main(String s[]) {  
+        JFrame frame = new JFrame("JFrame Example");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel("JFrame By Example");  
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);  
+    }  
+} 
