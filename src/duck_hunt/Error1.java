c7c0436c0c7c04b73e35299ace8e83566c69af9a
Vivek4075 <vivekkumarmeena4075@gmail.com>
2018-09-24 19:11:34 +0530

    game

---
+++ src/duck_hunt/Error1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duck_hunt;
+
+/**
+ *
+ * @author admin
+ */
+import acm.program.DialogProgram;
+public class Error1 extends DialogProgram
+{
+    public void run(String msg)
+    {
+    
+        println(msg);
+
+    }
+}
+
