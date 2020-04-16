afbb6b477f28fcd2963d8069b2e63df5222ed609
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-04-16 23:26:33 +0530

    Solved ducks spawn point problem

--- Server/src/DuckHunt/Request/GameState.java
+++ Server/src/DuckHunt/Request/GameState.java
@@ -15,11 +15,11 @@ public class GameState implements Serializable {
 		Y = new double[numOfDucks][numOfTrips+1];
 		for (int i = 0; i < numOfDucks; i++) {
 			for (int j = 0; j < numOfTrips; j++) {
-				X[i][j] = Math.random()*(1920 - 50);
-				Y[i][j] = 100 + Math.random()*(1080 - 50);
+				X[i][j] = Math.random()*(1500 - 50);
+				Y[i][j] = 100 + Math.random()*(980 - 50);
 			}
-			X[i][numOfTrips] = 1920 + Math.random()*(200);
-			Y[i][numOfTrips] = 1080 + Math.random()*(200);
+			X[i][numOfTrips] = 1920 + Math.random()*(300);
+			Y[i][numOfTrips] = 1080 + Math.random()*(300);
 			
 		}
 	}
